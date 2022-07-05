2d795223f53e153d8f42b8a81f1c78cb16e30176
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-07-05 22:33:45 +0100

    fix cors

--- app.js
+++ app.js
@@ -29,7 +29,13 @@ var corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200 // For legacy browser support
 }
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
